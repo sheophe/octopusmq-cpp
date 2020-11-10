3bfe80885d1ea41e2fb32bc4153c99108ba979d7
Paul Vakhnivskyi <pvakhnivskyi@gmail.com>
2020-11-10 19:57:59 +0200

    Build system improvements

--- build.sh
+++ build.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 if [ ! -d "./build" ]; then
-    mkdir ./build
+    mkdir build/
 fi
+
+OCTOMQ_OPT_FLAGS=""
+
+for var in "$@"
+do
+    case "$var" in
+        --optimize)
+            OCTOMQ_OPT_FLAGS="$OCTOMQ_OPT_FLAGS -D OCTOMQ_RELEASE_COMPILATION=ON"
+            ;;
+        --tls)
+            OCTOMQ_OPT_FLAGS="$OCTOMQ_OPT_FLAGS -D OCTOMQ_ENABLE_TLS=ON"
+            ;;
+        --clean)
+            rm -rf build/
+            mkdir build/
+            ;;
+    esac
+done
+
 cd ./build
-cmake ../
+cmake $OCTOMQ_OPT_FLAGS ../
 cmake --build . --target octopusmq -- -j 8
+
+unset OCTOMQ_OPT_FLAGS
