c498010b5dd8ebf87c15fd29e59c82c1b8a962a5
Paul Vakhnivskyi <pvakhnivskyi@gmail.com>
2020-12-03 19:31:08 +0200

    Replace std::filesystem with boost::filesystem

--- src/threads/control.cpp
+++ src/threads/control.cpp
@@ -8,6 +8,8 @@
 #include <stdexcept>
 #include <string>
 
+#include <boost/filesystem.hpp>
+
 #include "core/log.hpp"
 #include "core/settings.hpp"
 #include "core/utility.hpp"
@@ -87,12 +89,12 @@ void control::run(const int argc, const char **argv) {
                 else
                     throw std::runtime_error("unknown option: " + std::string(argv[i]));
             } else if (config_file_name.empty()) {
-                config_file_name = std::filesystem::absolute(argv[i]);
+                config_file_name = boost::filesystem::absolute(argv[i]).string();
                 if (config_file_name.empty())
                     throw std::runtime_error("not a valid file name: " + std::string(argv[i]));
-                if (not std::filesystem::exists(config_file_name))
+                if (not boost::filesystem::exists(config_file_name))
                     throw std::runtime_error("path does not exist: " + config_file_name);
-                if (not std::filesystem::is_regular_file(config_file_name))
+                if (not boost::filesystem::is_regular_file(config_file_name))
                     throw std::runtime_error("not a file: " + config_file_name);
                 settings::load(config_file_name, _adapter_pool);
             } else
