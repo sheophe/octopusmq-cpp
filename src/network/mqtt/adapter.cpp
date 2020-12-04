109df548caf3931e4a79bffbe72d8fd2ccbbfe96
Paul Vakhnivskyi <pvakhnivskyi@gmail.com>
2020-12-04 23:34:33 +0200

    Fix MQTT adapter name autogeneration

--- src/network/mqtt/adapter.cpp
+++ src/network/mqtt/adapter.cpp
@@ -16,7 +16,7 @@ adapter_settings::adapter_settings(const nlohmann::json &json)
 
     std::string transport_str = transport_field.get<std::string>();
     transport(transport_str);
-    name_append(std::string(mqtt::adapter_role_name::broker) + '(' + transport_str + ')');
+    name_append(std::string(mqtt::adapter_role_name::broker) + " (" + transport_str + ')');
 }
 
 void adapter_settings::transport(const transport_type &transport) { _transport = transport; }
