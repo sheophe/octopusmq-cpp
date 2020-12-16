d9274ede899c44ad5e1d139c46d3c93b78b54948
Paul Vakhnivskyi <pvakhnivskyi@gmail.com>
2020-12-16 15:44:33 +0200

    Disable DDS support by default

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ OCTOMQ_OPT_FLAGS=unset
 
 usage()
 {
-    echo "usage: build.sh [ -c | --clean ] [ -o | --optimize ] [ -s | --static ] [ -t | --tls ] [ --no-dds ]"
+    echo "usage: build.sh [ -c | --clean ] [ -o | --optimize ] [ -s | --static ] [ ---tls ] [ --dds ]"
     exit 2
 }
 
@@ -22,11 +22,11 @@ do
         -s | --static)
             OCTOMQ_OPT_FLAGS="$OCTOMQ_OPT_FLAGS -D OCTOMQ_USE_STATIC_LIBS=ON"
             ;;
-        -t | --tls)
+        --tls)
             OCTOMQ_OPT_FLAGS="$OCTOMQ_OPT_FLAGS -D OCTOMQ_ENABLE_TLS=ON"
             ;;
-        --no-dds)
-            OCTOMQ_OPT_FLAGS="$OCTOMQ_OPT_FLAGS -D OCTOMQ_ENABLE_DDS=OFF"
+        --dds)
+            OCTOMQ_OPT_FLAGS="$OCTOMQ_OPT_FLAGS -D OCTOMQ_ENABLE_DDS=ON"
             ;;
         --help)
             usage
